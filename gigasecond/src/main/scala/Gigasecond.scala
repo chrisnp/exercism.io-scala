7b5cab710584bf52eecb0966c699d96d7954831b
Invigo <invigo.devel@gmail.com>
2019-03-14 13:14:36 +0200

    gigasecond solution

---
+++ gigasecond/src/main/scala/Gigasecond.scala
@@ -0,0 +1,11 @@
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+object Gigasecond {
+
+  final val GIGASECOND = Math.pow(10, 9).toLong 
+
+  def add(startDate: LocalDate): LocalDateTime = startDate.atStartOfDay().plusSeconds(GIGASECOND)
+
+  def add(startDateTime: LocalDateTime): LocalDateTime = startDateTime.plusSeconds(GIGASECOND)
+}
