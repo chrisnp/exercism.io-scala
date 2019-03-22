dd0aff091441dc402cbfc3aa3e10e67685b06c16
Invigo <invigo.devel@gmail.com>
2019-03-22 16:40:41 +0200

    isogram solution

---
+++ isogram/src/main/scala/Isogram.scala
@@ -0,0 +1,9 @@
+object Isogram {
+
+    def isIsogram(str: String): Boolean = {
+        
+        val normalized = str.filter(_.isLetter).map(_.toLower)
+        normalized.distinct.size == normalized.size
+        
+    }
+}
