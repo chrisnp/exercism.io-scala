c753cdc8f8343d425d5f2aac7e715b4d717cc12c
Invigo <invigo.devel@gmail.com>
2019-04-12 00:31:21 +0300

    sieve refactor

--- sieve/src/main/scala/Sieve.scala
+++ sieve/src/main/scala/Sieve.scala
@@ -1,10 +1,22 @@
 object Sieve {
 
-    private def sieve: Seq[Int] => Seq[Int] = {
-        case head +: tail => head +: sieve(tail filter (_ % head > 0))
-        case Nil => Nil
+    private def sieve(num: Int): List[Int] = {
+        
+        // a stream of multiples of the odds up to the sq. root of num
+        val compositeOdds = Stream.from(3, 2)
+                                  .takeWhile(_ <= Math.sqrt(num).toInt)
+                                  .flatMap(x => Stream.from(x * x, 2 * x)
+                                  .takeWhile(_ <= num))
+
+        if (num <= 1) { 
+            List() // no primes below 2
+        } 
+        else {
+        // the primes up to a num are 2 and the not sieved non-composite odds less than num
+            2 :: Stream.from(3, 2).takeWhile(_ <= num).diff(compositeOdds).toList
+        }
     }
 
-    def primes(num: Int): Seq[Int] = sieve(2 to num)
+    def primes(num: Int): List[Int] = sieve(num)
 
 }
