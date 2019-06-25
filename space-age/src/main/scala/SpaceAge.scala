816dc3eab4a4a0dfb4a29d6a6035c9f576f74460
Invigo <invigo.devel@gmail.com>
2019-06-25 15:58:13 +0300

    space-age solution

---
+++ space-age/src/main/scala/SpaceAge.scala
@@ -0,0 +1,17 @@
+object SpaceAge {
+
+    private val round = (number:Double, precision:Int) =>
+        (number * (math pow (10, precision))).round / (math pow (10, precision))
+
+    private val planetAge = (seconds:Double, orbitalPeriod:Double) =>
+        round(seconds / (31557600 * orbitalPeriod), 2)
+
+    val onMercury = (seconds:Double) => planetAge(seconds, 0.2408467)
+    val onVenus   = (seconds:Double) => planetAge(seconds, 0.61519726)
+    val onEarth   = (seconds:Double) => planetAge(seconds, 1)
+    val onMars    = (seconds:Double) => planetAge(seconds, 1.8808158)
+    val onJupiter = (seconds:Double) => planetAge(seconds, 11.862615)
+    val onSaturn  = (seconds:Double) => planetAge(seconds, 29.447498)
+    val onUranus  = (seconds:Double) => planetAge(seconds, 84.016846)
+    val onNeptune = (seconds:Double) => planetAge(seconds, 164.79132)
+}
