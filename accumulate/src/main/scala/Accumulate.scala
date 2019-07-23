50b3093a44fa0674c301dd201a3f9b0f8309301c
Invigo <invigo.devel@gmail.com>
2019-07-23 18:57:32 +0300

    accumulate solution

---
+++ accumulate/src/main/scala/Accumulate.scala
@@ -0,0 +1,9 @@
+class Accumulate {
+
+    def accumulate[A, B] (f: (A) => B, list : List[A]): List[B] = {
+        list match {
+            case List() => Nil
+            case x::xs  => f(x)::accumulate(f, xs) 
+        }
+    }
+}
