f6c02ae4c7fdd104360e0690ce44a2184739bee1
Invigo <invigo.devel@gmail.com>
2019-10-01 17:45:37 +0300

    nucleotide-count solution 1st iteration

---
+++ nucleotide-count/src/main/scala/NucleotideCount.scala
@@ -0,0 +1,11 @@
+case class DNA(sequence : String) {
+  
+    val nucleotideCounts : Either[String, Map[Char, Int]] = {
+        
+        if (sequence.forall("ACGT".contains(_))) 
+            Right( Map('A' -> 0,'C' -> 0,'G' -> 0,'T' -> 0) 
+                   ++ sequence.groupBy(identity).mapValues(_.size) )
+        else
+            Left( "Invalid nucleotides in sequence" )
+    }
+}
