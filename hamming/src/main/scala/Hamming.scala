bd58e60925dd2748bc6876a3937d809891f39b6b
Invigo <invigo.devel@gmail.com>
2019-10-08 23:09:17 +0300

    refactored hamming solution's testRun

--- hamming/src/main/scala/Hamming.scala
+++ hamming/src/main/scala/Hamming.scala
@@ -2,10 +2,9 @@ object Hamming {
 
     def distance(strand1 : String, strand2 : String) : Option[Int] = {
 
-        if (strand1.length() != strand2.length()) return None 
- 
-        Some((strand1, strand2)
-             .zipped.map(_==_)
-             .count(_ == false))
+        if (strand1.length() != strand2.length()) None 
+        else Some((strand1, strand2)
+                  .zipped.map(_==_)
+                  .count(_ == false))
     }
 }
