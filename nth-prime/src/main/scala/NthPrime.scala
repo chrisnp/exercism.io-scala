bab68d7cb9ded2cd6726f1c908f845ad3f257ef0
Invigo <invigo.devel@gmail.com>
2019-10-18 19:19:53 +0300

    nth-prime refactor

--- nth-prime/src/main/scala/NthPrime.scala
+++ nth-prime/src/main/scala/NthPrime.scala
@@ -1,11 +1,11 @@
 object NthPrime {
 
     private def isPrime(n : Int) :  Boolean =
-        (2 to Math.sqrt(n).toInt) forall (n % _ != 0) 
+        2 to (Math.sqrt(n) toInt) forall (n % _ != 0) 
     
     private lazy val primes : Stream[Int] = 
         2 #:: (Stream.from(3, 2) filter isPrime)
 
     def prime(n : Int) : Option[Int] = 
-        if (n < 1) None else Some (primes take(n) last)
+        if (n < 1) None else Some (primes take n last)
 }
