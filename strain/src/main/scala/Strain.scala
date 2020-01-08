4bec9d93eb8f1c1f68de9ddab067aa7c2bdd90db
Zero Delta <zerodeltahedge@gmail.com>
2020-01-08 23:48:46 +0200

    Refactor Strain.scala

--- strain/src/main/scala/Strain.scala
+++ strain/src/main/scala/Strain.scala
@@ -1,11 +1,11 @@
 object Strain {
 
     def keep[A](xs : Seq[A], f : A => Boolean) : Seq[A] = {
-        for (x : A <- xs if f(x)) yield x
+        for (x <- xs if f(x)) yield x
     }
 
     def discard[A](xs : Seq[A], f : A => Boolean) : Seq[A] = {
-        for (x : A <- xs if !f(x)) yield x
+        for (x <- xs if !f(x)) yield x
     }
 
 }
