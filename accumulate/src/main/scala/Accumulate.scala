e759e01cb76c725d79da1cc7b6872d1bd33fe704
Invigo <invigo.devel@gmail.com>
2021-02-03 02:21:48 +0200

    Update Accumulate.scala

--- accumulate/src/main/scala/Accumulate.scala
+++ accumulate/src/main/scala/Accumulate.scala
@@ -1,9 +1,10 @@
 class Accumulate {
 
-    def accumulate[A, B] (f: (A) => B, list : List[A]): List[B] = {
-        list match {
-            case List() => Nil
-            case x::xs  => f(x)::accumulate(f, xs) 
-        }
+  def accumulate[A, B] (f: (A) => B, 
+                        list: List[A]): List[B] = {
+    list match {
+      case List() => Nil
+      case x::xs  => f(x)::accumulate(f, xs) 
     }
+  }
 }
