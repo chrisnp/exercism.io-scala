136bdd566ac0a3e855ff641ccd06fb3e76088122
Invigo <invigo.devel@gmail.com>
2021-02-04 18:50:03 +0200

    Update BeerSong.scala

--- beer-song/src/main/scala/BeerSong.scala
+++ beer-song/src/main/scala/BeerSong.scala
@@ -2,26 +2,43 @@ object BeerSong {
 
     private def firstLine(bottles: Int): String = {
         bottles match {
-            case 0 => "No more bottles of beer on the wall, no more bottles of beer."
-            case 1 => "1 bottle of beer on the wall, 1 bottle of beer."
-            case _ => s"$bottles bottles of beer on the wall, $bottles bottles of beer."
+            case 0 => 
+                "No more bottles of beer on the wall, " + 
+                "no more bottles of beer."
+            case 1 => 
+                "1 bottle of beer on the wall, " + 
+                "1 bottle of beer."
+            case _ => 
+                s"$bottles bottles of beer on the wall, " + 
+                s"$bottles bottles of beer."
         }
     }
 
     private def secondLine(bottles: Int): String = {
         bottles match {
-            case 0 => "Go to the store and buy some more, 99 bottles of beer on the wall."
-            case 1 => "Take it down and pass it around, no more bottles of beer on the wall."
-            case 2 => s"Take one down and pass it around, ${bottles - 1} bottle of beer on the wall."
-            case _ => s"Take one down and pass it around, ${bottles - 1} bottles of beer on the wall."
+            case 0 => 
+                "Go to the store and buy some more, " + 
+                "99 bottles of beer on the wall."
+            case 1 => 
+                "Take it down and pass it around, " + 
+                "no more bottles of beer on the wall."
+            case 2 => 
+                "Take one down and pass it around, " + 
+                s"${bottles - 1} bottle of beer on the wall."
+            case _ => 
+                "Take one down and pass it around, " + 
+                s"${bottles - 1} bottles of beer on the wall."
         }
     }
 
     private def verse(bottles: Int): String = {
-        firstLine(bottles) + "\n" + secondLine(bottles) + "\n"
+        firstLine(bottles) + "\n" + 
+        secondLine(bottles) + "\n"
     }
 
     def recite(beersUp:Int, beersDown:Int): String = {
-        (beersUp until (beersUp - beersDown) by -1).map(bottles => verse(bottles)).mkString("\n")
+        (beersUp until (beersUp - beersDown) by -1)
+        .map(bottles => 
+            verse(bottles)).mkString("\n")
     }
  }
