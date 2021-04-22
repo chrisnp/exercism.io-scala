766ecbc4b9f6797b649847ea4582cb27ba1a8544
Zero Delta <zerodeltahedge@gmail.com>
2021-04-22 17:27:49 +0300

    Revert "Update BinarySearch.scala"
    This reverts commit dce957cafdd8f21ccde57373ea0ffcb386e2d086.

--- binary-search/src/main/scala/BinarySearch.scala
+++ binary-search/src/main/scala/BinarySearch.scala
@@ -9,12 +9,11 @@ object BinarySearch {
             val half = min + size / 2
             val mid = haystaq apply half
 
-            if (haystaq(mid) == `needle`) 
-                Some(mid)
-            else if (mid > needle) 
-                search(min, mid - 1)
-            else 
-                search(mid + 1, max)
+            haystaq(mid) match {
+                case `needle` => Some(mid)
+                case x if x > needle => search(min, mid - 1)
+                case x if x < needle => search(mid + 1, max) 
+            }
         }
         
         search(0, haystaq.length - 1)
