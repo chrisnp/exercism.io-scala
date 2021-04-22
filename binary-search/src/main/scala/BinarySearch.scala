38fb13a1e4240c24e46c3863c27ed42d887c7c79
Invigo <invigo.devel@gmail.com>
2021-04-22 17:30:40 +0300

    Update BinarySearch.scala

--- binary-search/src/main/scala/BinarySearch.scala
+++ binary-search/src/main/scala/BinarySearch.scala
@@ -9,11 +9,12 @@ object BinarySearch {
             val half = min + size / 2
             val mid = haystaq apply half
 
-            haystaq(mid) match {
-                case `needle` => Some(mid)
-                case x if x > needle => search(min, mid - 1)
-                case x if x < needle => search(mid + 1, max) 
-            }
+            if (haystaq(mid) == `needle`) 
+                Some(mid)
+            else if (mid > needle) 
+                search(min, mid - 1)
+            else 
+                search(mid + 1, max)
         }
         
         search(0, haystaq.length - 1)
