11dbfa644abdbedf949840f99091b97911c2df61
Invigo <invigo.devel@gmail.com>
2021-04-24 04:08:01 +0300

    Refactor BinarySearch.scala

--- binary-search/src/main/scala/BinarySearch.scala
+++ binary-search/src/main/scala/BinarySearch.scala
@@ -1,19 +1,15 @@
 object BinarySearch {
-
+        
     def find(haystaq: Seq[Int], needle: Int): Option[Int] = {
-        
-        @annotation.tailrec
         def search(min: Int, max: Int): Option[Int] = {
-            if (min >= max) return None
             val size = max - min
-            val half = min + size / 2
-            val mid = haystaq apply half
-
-            if (haystaq(mid) == `needle`) Some(mid)
-            else if (mid > needle) search(min, mid - 1)
-            else search(mid + 1, max)
+            val mid = min + size / 2 
+            if (min >= max) None
+            else if (haystaq(mid) == needle) Some(mid)
+            else search(min, mid)
+                 .orElse(search(mid + 1, max))
         }
-        
-        search(0, haystaq.length - 1)
+        if (haystaq.isEmpty) None 
+        else search(0, haystaq.size)
     }
 }
