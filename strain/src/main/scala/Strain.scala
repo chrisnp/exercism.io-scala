5d0e8f9fe7fab58cfb124287f949dc8895126075
Invigo <invigo.devel@gmail.com>
2021-06-05 15:13:18 +0300

    Refactor Strain.scala

--- strain/src/main/scala/Strain.scala
+++ strain/src/main/scala/Strain.scala
@@ -1,11 +1,13 @@
 object Strain {
 
-    def keep[A](xs : Seq[A], f : A => Boolean) : Seq[A] = {
+    def keep[A](xs : Seq[A], f : A => Boolean) : Seq[A] = 
+    {
         for (x <- xs if f(x)) yield x
     }
 
-    def discard[A](xs : Seq[A], f : A => Boolean) : Seq[A] = {
-        for (x <- xs if !f(x)) yield x
+    def discard[A](xs : Seq[A], f : A => Boolean) : Seq[A] = 
+    {
+        keep(xs, !f(_: A))
     }
 
 }
