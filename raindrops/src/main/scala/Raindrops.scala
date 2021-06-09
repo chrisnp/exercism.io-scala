89d4b77d4c2039c73d87ebb5d180b87a59fe2418
Invigo <invigo.devel@gmail.com>
2021-06-10 02:37:38 +0300

    raindrops solution

--- raindrops/src/main/scala/Raindrops.scala
+++ raindrops/src/main/scala/Raindrops.scala
@@ -1,5 +1,19 @@
 object Raindrops {
-  def convert(n: Int): String = ???
-  
-}
 
+  def convert(n: Int): String = {
+
+    val dropz = 
+      Map[Int, String](
+          3 -> "Pling", 
+          5 -> "Plang", 
+          7 -> "Plong")
+      .filter(n % _._1 == 0)
+      .values
+      .reduceOption {_++_}
+      .getOrElse("")
+
+    if (dropz.isEmpty) n.toString
+    else dropz
+
+  } 
+}
