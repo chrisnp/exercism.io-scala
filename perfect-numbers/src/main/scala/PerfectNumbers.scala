f956feff9cbae39aa2ddef16d4abc917f0cb2925
Invigo <invigo.devel@gmail.com>
2021-07-31 01:52:52 +0300

    PerfectNumbers.scala 1st working iteration

--- perfect-numbers/src/main/scala/PerfectNumbers.scala
+++ perfect-numbers/src/main/scala/PerfectNumbers.scala
@@ -1,3 +1,18 @@
 object PerfectNumbers {
-    
+    import NumberType._
+
+    def classify(number: Int): Either[String, NumberType] = {
+        if (number <= 0) 
+            Left("Classification is only possible for natural numbers.")
+        else aliquotSum(number) match {
+            case a if a > number => Right(NumberType.Abundant)
+            case d if d < number => Right(NumberType.Deficient)
+            case _ => Right(NumberType.Perfect)
+        }
+    }
+
+    def aliquotSum(number: Int): Int =
+        (1 to number / 2)
+        .filter {number % _ == 0} 
+        .foldLeft(0) {_+_}
 }
