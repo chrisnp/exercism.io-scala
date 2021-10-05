a8352841d9f5e1787fc7a1348783309ae0d65d86
Invigo <invigo.devel@gmail.com>
2021-10-05 23:28:59 +0300

    Reformat roman-numerals

--- roman-numerals/src/main/scala/RomanNumerals.scala
+++ roman-numerals/src/main/scala/RomanNumerals.scala
@@ -10,46 +10,46 @@ object RomanNumerals {
       arabic match {
         
         case a if a >= 1000 => 
-                roman.append("M");  
+                roman append("M");  
                 arabic -= 1000
         case a if a >= 900 => 
-                roman.append("CM"); 
+                roman append("CM"); 
                 arabic -= 900
         case a if a >= 500 => 
-                roman.append("D"); 
+                roman append("D"); 
                 arabic -= 500
         case a if a >= 400 => 
-                roman.append("CD"); 
+                roman append("CD"); 
                 arabic -= 400
         case a if a >= 100 => 
-                roman.append("C"); 
+                roman append("C"); 
                 arabic -= 100
         case a if a >= 90 => 
-                roman.append("XC"); 
+                roman append("XC"); 
                 arabic -= 90
         case a if a >= 50 => 
-                roman.append("L"); 
+                roman append("L"); 
                 arabic -= 50
         case a if a >= 40 => 
-                roman.append("XL"); 
+                roman append("XL"); 
                 arabic -= 40
         case a if a >= 10 => 
-                roman.append("X"); 
+                roman append("X"); 
                 arabic -= 10
         case a if a >= 9 => 
-                roman.append("IX"); 
+                roman append("IX"); 
                 arabic -= 9
         case a if a >= 5 => 
-                roman.append("V"); 
+                roman append("V"); 
                 arabic -= 5
         case a if a >= 4 => 
-                roman.append("IV"); 
+                roman append("IV"); 
                 arabic -= 4
         case a if a >  0 => 
-                roman.append("I"); 
+                roman append("I"); 
                 arabic -= 1
       }
     }
-    roman.toString()
+    roman toString()
   }
 }
