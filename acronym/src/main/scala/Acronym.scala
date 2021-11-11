ad16fb70c3ee18193a5952dd56afb996b41b7b24
Invigo <invigo.devel@gmail.com>
2021-11-11 17:26:09 +0200

    Refactor Acronym.scala

--- acronym/src/main/scala/Acronym.scala
+++ acronym/src/main/scala/Acronym.scala
@@ -1,7 +1,5 @@
 object Acronym {
 
-  def abbreviate(phrase: String): String = {
-    
+  def abbreviate(phrase: String): String =
     "(\\w|\')+".r findAllIn(phrase) map(x => x(0) toUpper) mkString
-  }
 }
