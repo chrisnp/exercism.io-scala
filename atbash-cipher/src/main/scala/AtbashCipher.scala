1b546a071899029c5b331b582ca24894e0bff6f1
Invigo <invigo.devel@gmail.com>
2021-11-30 23:19:04 +0200

    Update AtbashCipher.scala

--- atbash-cipher/src/main/scala/AtbashCipher.scala
+++ atbash-cipher/src/main/scala/AtbashCipher.scala
@@ -7,16 +7,10 @@ object AtbashCipher {
         (alpha.zip(alpha reverse) ++: (numeric.zip(numeric))) toMap
 
     def encode(string: String): String = 
-        string.filter(_ isLetterOrDigit)
-              .map(_ toLower)
-              .map(shift)
-              .sliding(5, 5)
-              .map(_ mkString)
-              .mkString(" ") 
+        string.filter(_ isLetterOrDigit).map(_ toLower)
+              .map(shift).sliding(5, 5).map(_ mkString) mkString(" ") 
 
     def decode(string: String): String =
-        string.filter(!_.isSpaceChar)
-              .map(shift)
-              .mkString 
+        string filter(!_.isSpaceChar) map(shift) mkString 
 
 }
