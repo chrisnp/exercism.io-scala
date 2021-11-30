462ce8314cd1ff2388e99a240a6d899345150e81
Invigo <invigo.devel@gmail.com>
2021-11-30 23:27:37 +0200

    Reformat AtbashCipher.scala

--- atbash-cipher/src/main/scala/AtbashCipher.scala
+++ atbash-cipher/src/main/scala/AtbashCipher.scala
@@ -4,13 +4,19 @@ object AtbashCipher {
     private val numeric = ('0' to '9')
 
     val shift = 
-        (alpha.zip(alpha reverse) ++: (numeric.zip(numeric))) toMap
+        (alpha.zip(alpha reverse) ++: 
+        (numeric.zip(numeric))) toMap
 
     def encode(string: String): String = 
-        string.filter(_ isLetterOrDigit).map(_ toLower)
-              .map(shift).sliding(5, 5).map(_ mkString) mkString(" ") 
+        string.filter(_ isLetterOrDigit)
+              .map(_ toLower) 
+              .map(shift).sliding(5, 5)
+              .map(_ mkString) 
+              .mkString(" ") 
 
     def decode(string: String): String =
-        string filter(!_.isSpaceChar) map(shift) mkString 
+        string.filter(!_.isSpaceChar)
+              .map(shift)
+              .mkString 
 
 }
