b45cfbee10665e1cb63dd7b6d89bd6c5e56719ce
Invigo <invigo.devel@gmail.com>
2022-05-05 03:38:07 +0300

    Refactor  Luhn.scala

--- luhn/src/main/scala/Luhn.scala
+++ luhn/src/main/scala/Luhn.scala
@@ -1,27 +1,26 @@
-class Luhn (val value: Long){
-    private def addend(di: (Char, Int)) = 
-        di match {
-            case (d, i) if i % 2 == 0 => d.asDigit
-            case (d, _) if d.asDigit > 4 => 2 * d.asDigit - 9
-            case (d, _) => 2 * d.asDigit 
-        }
-    
-    def addends = value.toString.reverse.zipWithIndex.map(addend).reverse
-
-    def checkDigit = value % 10
-
-    def checkSum = addends.sum % 10
-
-    def isValid = checkSum == 0
-
-    lazy val create = (0 to 9)
-                .map(10 * value + _)
-                // .map(_.isValid)
-                .find(_.isValid)
-                .get
-                .value
-}
-
 object Luhn {
-    def valid(number: Long) = new Luhn(number);
+    import scala.util.Try
+
+     def valid(number: String): Boolean = 
+        Try {
+            val sanitary = number.replaceAll(" ", "") 
+            sanitary.length > 1 && 
+            sanitary.map { case d if d.isDigit => d
+                           case _ => throw new Exception(
+                                            "not a digit"
+                                           )}
+                    .reverse
+                    .zipWithIndex
+                    .map(id => (id._2, id._1))
+                    .map(addend _)
+                    .reverse
+                    .sum % 10 == 0
+        }.getOrElse(false)  
+
+    private def addend(indexedDigit: (Int, Char)) = 
+        indexedDigit match {
+            case (i, d) if i % 2 == 0 => d.asDigit
+            case (_, d) if d.asDigit > 4 => 2 * d.asDigit - 9
+            case (_, d) => 2 * d.asDigit 
+        }
 }
