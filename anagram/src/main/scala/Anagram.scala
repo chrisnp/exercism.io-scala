468192e375990dfaac1cd6399eac024122a26e82
Invigo <invigo.devel@gmail.com>
2022-06-07 23:46:42 +0300

    Update Anagram.scala

--- anagram/src/main/scala/Anagram.scala
+++ anagram/src/main/scala/Anagram.scala
@@ -1,12 +1,7 @@
 object Anagram {
-
-    def findAnagrams(word: String, 
-                     candidates: Seq[String]): Seq[String] = 
-    {
+    def findAnagrams(word: String, candidates: Seq[String]): Seq[String] = {
         candidates
         .filterNot(_ equalsIgnoreCase word)
-        .filter(_.sortBy(_.toLower) 
-                equalsIgnoreCase 
-                word.sortBy(_.toLower))
+        .filter(_ sortBy(_ toLower) equalsIgnoreCase (word sortBy(_ toLower)))
     }
 }
