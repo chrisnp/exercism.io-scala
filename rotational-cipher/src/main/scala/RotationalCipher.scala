d9b23f3216ae39b116c6f07d9e2c55b74e044512
Invigo <invigo.devel@gmail.com>
2022-06-22 02:40:18 +0300

    RotationalCipher.scala - passing all tests

--- rotational-cipher/src/main/scala/RotationalCipher.scala
+++ rotational-cipher/src/main/scala/RotationalCipher.scala
@@ -1,3 +1,15 @@
 object RotationalCipher {
     
+    def rotate(char: Char, shift: Int): Char = {
+        val base = if (char.isLower) 'a' else 'A'
+        (base + (char - base + shift) % 26)
+        .toChar 
+    }
+
+    def rotate(plain: String, shift: Int): String = {
+        if (shift == 0) return plain
+        plain
+        .map(ch => if (!ch.isLetter) ch else rotate(ch, shift))
+        .mkString   
+    }
 }
