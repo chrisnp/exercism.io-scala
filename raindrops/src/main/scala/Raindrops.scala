1648965050c665402b98c70cf24c9d1773d508a4
Invigo <invigo.devel@gmail.com>
2022-07-08 02:13:09 +0300

    Reformat Raindrops.scala

--- raindrops/src/main/scala/Raindrops.scala
+++ raindrops/src/main/scala/Raindrops.scala
@@ -1,19 +1,14 @@
 object Raindrops {
 
   def convert(n: Int): String = {
-
-    val dropz = 
-      Map[Int, String](
-          3 -> "Pling", 
-          5 -> "Plang", 
-          7 -> "Plong")
-      .filter(n % _._1 == 0)
-      .values
-      .reduceOption {_++_}
-      .getOrElse("")
-
+    val dropz = Map[Int, String]( 3 -> "Pling", 
+                                  5 -> "Plang", 
+                                  7 -> "Plong")
+                .filter(n % _._1 == 0)
+                .values
+                .reduceOption {_++_}
+                .getOrElse("")
     if (dropz.isEmpty) n.toString
     else dropz
-
   } 
 }
