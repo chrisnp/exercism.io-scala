203ca40a3460b0a0532351815d702ef596d782fd
Invigo <invigo.devel@gmail.com>
2022-07-12 02:06:24 +0300

    Update SeriesTest.scala

--- series/src/test/scala/SeriesTest.scala
+++ series/src/test/scala/SeriesTest.scala
@@ -1,7 +1,9 @@
-import org.scalatest.{Matchers, FunSuite}
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
 
 /** @version created manually **/
-class SeriesTest extends FunSuite with Matchers {
+class SeriesTest extends AnyFunSuite with Matchers {
 
   test("slices of one") {
     Series.slices(1, "") should be (List())
@@ -10,7 +12,7 @@ class SeriesTest extends FunSuite with Matchers {
   }
 
   test("slices of two") { 
-    // pending
+    pending
     Series.slices(2, "") should be (List())
     Series.slices(2, "01") should be (List(List(0, 1)))
     Series.slices(2, "01234") should be (List(List(0, 1), List(1, 2), List(2, 3),
@@ -18,7 +20,7 @@ class SeriesTest extends FunSuite with Matchers {
   }
 
   test("slices of three") { 
-    // pending
+    pending
     Series.slices(3, "") should be (List())
     Series.slices(3, "012") should be (List(List(0, 1, 2)))
     Series.slices(3, "01234") should be (List(List(0, 1, 2), List(1, 2, 3),
