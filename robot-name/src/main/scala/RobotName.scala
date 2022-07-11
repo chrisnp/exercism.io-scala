cc56b35e0b408efb3197e46ec55ed782a99a32f3
Invigo <invigo.devel@gmail.com>
2022-07-11 17:31:09 +0300

    Reformat RobotName.scala

--- robot-name/src/main/scala/RobotName.scala
+++ robot-name/src/main/scala/RobotName.scala
@@ -1,17 +1,16 @@
 class Robot {
-
     import Robot._
-    
     var name: String = namesGen.next()
-   
     def reset(): Unit = name = namesGen.next()
 }
 
 object Robot {
-
     import scala.util.{Random => r}
-
-    val namesGen: Iterator[String] = r.shuffle(
-        for( a1 <- 'A' to 'Z'; a2 <- 'A' to 'Z'; ds <- 0 to 999) 
-            yield {s"$a1$a2$ds"}).iterator
+    val namesGen: Iterator[String] = 
+        r.shuffle( for ( a1 <- 'A' to 'Z'; 
+                         a2 <- 'A' to 'Z'; 
+                         ds <- 0 to 999 ) 
+                         yield { s"$a1$a2$ds" } 
+                 )
+         .iterator
 }
