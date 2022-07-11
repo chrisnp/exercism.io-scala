00087c34a80f0fdae4a6ccfae92fab3e5c9cab8c
Invigo <invigo.devel@gmail.com>
2022-07-12 02:12:38 +0300

    Update SeriesTest.scala

--- series/src/test/scala/SeriesTest.scala
+++ series/src/test/scala/SeriesTest.scala
@@ -12,7 +12,7 @@ class SeriesTest extends AnyFunSuite with Matchers {
   }
 
   test("slices of two") { 
-    pending
+    // pending
     Series.slices(2, "") should be (List())
     Series.slices(2, "01") should be (List(List(0, 1)))
     Series.slices(2, "01234") should be (List(List(0, 1), List(1, 2), List(2, 3),
@@ -20,7 +20,7 @@ class SeriesTest extends AnyFunSuite with Matchers {
   }
 
   test("slices of three") { 
-    pending
+    // pending
     Series.slices(3, "") should be (List())
     Series.slices(3, "012") should be (List(List(0, 1, 2)))
     Series.slices(3, "01234") should be (List(List(0, 1, 2), List(1, 2, 3),
