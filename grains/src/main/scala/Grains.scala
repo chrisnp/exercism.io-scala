f5b40b4e0abf611b3fdd35dfd576572fcc9ab0c4
Invigo <invigo.devel@gmail.com>
2022-08-26 20:12:59 +0300

    Update Grains.scala

--- grains/src/main/scala/Grains.scala
+++ grains/src/main/scala/Grains.scala
@@ -1,9 +1,12 @@
 object Grains {
     def square(num: Int): Option[BigInt] = {
-        if ( num < 1 || num > 64 ) { return None }
-        Some ( BigInt(1) << num - 1 )
+        if (num < 1 || num > 64) {
+            return None
+        }
+        Some (BigInt(1) << num - 1)
     }
+
     def total(): BigInt = {
-        ( BigInt(1) << 64 ) - 1
+        (BigInt(1) << 64) - 1
     }
 }
