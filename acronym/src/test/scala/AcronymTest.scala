c08a337d2eea6a9cc330d49959cf7805588b4248
Invigo <invigo.devel@gmail.com>
2022-08-29 23:00:44 +0300

    Update AcronymTest.scala

--- acronym/src/test/scala/AcronymTest.scala
+++ acronym/src/test/scala/AcronymTest.scala
@@ -1,44 +1,46 @@
-import org.scalatest.{Matchers, FunSuite}
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
 
 /** @version 1.6.0 */
-class AcronymTest extends FunSuite with Matchers {
+class AcronymTest extends AnyFunSuite with Matchers {
 
   test("basic") {
     Acronym.abbreviate("Portable Network Graphics") should be ("PNG")
   }
 
   test("lowercase words") {
-    // pending
+    pending
     Acronym.abbreviate("Ruby on Rails") should be ("ROR")
   }
 
   test("punctuation") {
-    // pending
+    pending
     Acronym.abbreviate("First In, First Out") should be ("FIFO")
   }
 
   test("all caps word") {
-    // pending
+    pending
     Acronym.abbreviate("GNU Image Manipulation Program") should be ("GIMP")
   }
 
   test("punctuation without whitespace") {
-    // pending
+    pending
     Acronym.abbreviate("Complementary metal-oxide semiconductor") should be ("CMOS")
   }
 
   test("very long abbreviation") {
-    // pending
+    pending
     Acronym.abbreviate("Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me") should be ("ROTFLSHTMDCOALM")
   }
 
   test("consecutive delimiters") {
-    // pending
+    pending
     Acronym.abbreviate("Something - I made up from thin air") should be ("SIMUFTA")
   }
 
   test("apostrophes") {
-    // pending
+    pending
     Acronym.abbreviate("Halley's Comet") should be ("HC")
   }
 }
