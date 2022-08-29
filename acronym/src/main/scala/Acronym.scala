458d93897c09ab142071611fcf5d3cba791a1709
Invigo <invigo.devel@gmail.com>
2022-08-29 23:00:52 +0300

    Update Acronym.scala

--- acronym/src/main/scala/Acronym.scala
+++ acronym/src/main/scala/Acronym.scala
@@ -1,4 +1,7 @@
 object Acronym {
-  def abbreviate(phrase: String): String =
+
+  def abbreviate(phrase: String): String = {
+    
     "(\\w|\')+".r findAllIn(phrase) map(x => x(0) toUpper) mkString
+  }
 }
