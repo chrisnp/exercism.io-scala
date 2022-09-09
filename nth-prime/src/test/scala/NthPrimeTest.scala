439140e9c497ebb53065ce4060a794d5409de6dc
Invigo <invigo.devel@gmail.com>
2022-09-09 23:14:56 +0300

    Update NthPrimeTest.scala

--- nth-prime/src/test/scala/NthPrimeTest.scala
+++ nth-prime/src/test/scala/NthPrimeTest.scala
@@ -1,29 +1,31 @@
-import org.scalatest.{Matchers, FunSuite}
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
 
 /** @version 2.1.0 */
-class NthPrimeTest extends FunSuite with Matchers {
+class NthPrimeTest extends AnyFunSuite with Matchers {
   
   test("first prime") {
     NthPrime.prime(1) should be (Some(2))
   }
 
   test("second prime") {
-    // pending
+    pending
     NthPrime.prime(2) should be (Some(3))
   }
 
   test("sixth prime") {
-    // pending
+    pending
     NthPrime.prime(6) should be (Some(13))
   }
 
   test("big prime") {
-    // pending
+    pending
     NthPrime.prime(10001) should be (Some(104743))
   }
 
   test("there is no zeroth prime") {
-    // pending
+    pending
     NthPrime.prime(0) should be (None)
   }
 }
