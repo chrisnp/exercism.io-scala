f908fa1e83d7f9e655ef2ccf497be6c8e9805629
Invigo <invigo.devel@gmail.com>
2022-09-16 23:51:03 +0300

    Update ProteinTranslation.scala

--- protein-translation/src/main/scala/ProteinTranslation.scala
+++ protein-translation/src/main/scala/ProteinTranslation.scala
@@ -1,16 +1,18 @@
 object ProteinTranslation {
+    import scala.language.postfixOps
 
     private def translate(codon: String): String =
         codon match {
-            case "AUG"                         => "Methionine"
-            case "UUU" | "UUC"                 => "Phenylalanine"
-            case "UUA" | "UUG"                 => "Leucine"
+            case "AUG" => "Methionine"
+            case "UUU" | "UUC" => "Phenylalanine"
+            case "UUA" | "UUG" => "Leucine"
             case "UCU" | "UCC" | "UCA" | "UCG" => "Serine"
-            case "UAU" | "UAC"                 => "Tyrosine"
-            case "UGU" | "UGC"                 => "Cysteine"
-            case "UGG"                         => "Tryptophan"
-            case "UAA" | "UAG" | "UGA"         => "STOP"
-            case _ => throw new IllegalArgumentException("Unknown codon")
+            case "UAU" | "UAC" => "Tyrosine"
+            case "UGU" | "UGC" => "Cysteine"
+            case "UGG" => "Tryptophan"
+            case "UAA" | "UAG" | "UGA" => "STOP"
+            case _ => 
+                throw new IllegalArgumentException("Unknown codon")
         }
 
     def proteins(strand: String): Seq[String] = 
