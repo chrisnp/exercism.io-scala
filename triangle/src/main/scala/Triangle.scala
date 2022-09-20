1bb211c4b0996bf7619d4519602e918a3778209d
Invigo <invigo.devel@gmail.com>
2022-09-20 23:53:26 +0300

    Update Triangle.scala

--- triangle/src/main/scala/Triangle.scala
+++ triangle/src/main/scala/Triangle.scala
@@ -1,17 +1,15 @@
 case class Triangle(a: Double, b: Double, c: Double) {
-    
+
     def triangleInequality: Boolean = 
         Set(a, b, c).min > 0 && 
-        (a <= b + c) && (b <= c + a) && (c <= a + b) 
+        2 * Seq(a, b, c).max <= Seq(a, b, c).sum 
 
     def equalSides: Int = 4 - Set(a, b, c).size
 
-    def equilateral: Boolean =
+    def equilateral: Boolean = 
         triangleInequality && (equalSides == 3)
-
     def isosceles: Boolean = 
         triangleInequality && (equalSides >= 2)
-
     def scalene: Boolean = 
         triangleInequality && (equalSides == 1)
 }
