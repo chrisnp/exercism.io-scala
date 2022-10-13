c4ac2d2fa11fdf4e7a84e3b68bb3b37debe2681e
Invigo <invigo.devel@gmail.com>
2022-10-13 21:38:45 +0300

    Refactor word-count

--- word-count/src/main/scala/WordCount.scala
+++ word-count/src/main/scala/WordCount.scala
@@ -1,10 +1,9 @@
 case class WordCount(private val phrase : String) {
-
+    import scala.language.postfixOps
+    
     private lazy val words = 
-        phrase.toLowerCase.split("\\W'|'\\W|[^\\w']+")
-              .filter(!_.isEmpty)
-
-    def countWords() : Map[String, Int] = 
-        words.groupBy(identity).view.mapValues(_.size)
-             .toMap
+        phrase.toLowerCase.split("\\W'|'\\W|[^\\w']+") filter(!_.isEmpty)
+    
+    def countWords(): Map[String, Int] = 
+        words groupBy(identity) map {case(word, count) => (word, count.size)}
 }
