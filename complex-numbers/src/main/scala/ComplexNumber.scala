8bd358d43e9c85d7b6b163bdb9159812a5946474
Invigo <invigo.devel@gmail.com>
2022-10-13 21:19:16 +0300

    Refactor complex-numbers

--- complex-numbers/src/main/scala/ComplexNumber.scala
+++ complex-numbers/src/main/scala/ComplexNumber.scala
@@ -1,35 +1,24 @@
 import scala.math.{cos, sin, sqrt, exp => xp}
 
-case class ComplexNumber(real:Double = 0.0, 
-                         imaginary:Double = 0.0) {
-
-    lazy val abs:Double = 
-        sqrt(real * real + imaginary * imaginary)
-
-    lazy val conjugate:ComplexNumber = 
-        ComplexNumber(real, -imaginary)
-
-    def + (z:ComplexNumber):ComplexNumber = 
-        ComplexNumber(real + z.real, imaginary + z.imaginary)
-
-    def - (z:ComplexNumber):ComplexNumber = 
-        ComplexNumber(real - z.real, imaginary - z.imaginary)
-
-    def * (z:ComplexNumber):ComplexNumber =
-        ComplexNumber(real * z.real - imaginary * z.imaginary, 
-                      imaginary * z.real + real * z.imaginary)
-
-    def / (z:ComplexNumber):ComplexNumber = 
-        ComplexNumber((real * z.real + imaginary * z.imaginary) 
-                      / (z.abs * z.abs), 
-                      (imaginary * z.real - real * z.imaginary) 
-                      / (z.abs * z.abs))
+case class ComplexNumber(real:Double = 0.0, imaginary:Double = 0.0) {
+    lazy val abs: Double = sqrt(real * real + imaginary * imaginary)
+    lazy val conjugate:ComplexNumber = ComplexNumber(real, -imaginary)
+    def + (z: ComplexNumber): ComplexNumber = 
+                ComplexNumber(real + z.real, imaginary + z.imaginary)
+    def - (z: ComplexNumber): ComplexNumber = 
+                ComplexNumber(real - z.real, imaginary - z.imaginary)
+    def * (z: ComplexNumber): ComplexNumber =
+            ComplexNumber(real * z.real - imaginary * z.imaginary, 
+                          imaginary * z.real + real * z.imaginary)
+    def / (z: ComplexNumber): ComplexNumber = { 
+        val denom = z.abs * z.abs
+        ComplexNumber((real * z.real + imaginary * z.imaginary) / denom, 
+                      (imaginary * z.real - real * z.imaginary) / denom)
+    }
 }
 
 object ComplexNumber {
-
-    def exp(z:ComplexNumber):ComplexNumber = 
-            ComplexNumber(xp(z.real) * cos(z.imaginary), 
-                          xp(z.real) * sin(z.imaginary))
-
+    def exp(z: ComplexNumber): ComplexNumber = 
+        ComplexNumber(xp(z.real) * cos(z.imaginary), 
+                      xp(z.real) * sin(z.imaginary))
 }
