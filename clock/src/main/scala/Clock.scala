4bde07f0d6c01fb749f68cd7a5d9fae720b35369
Invigo <invigo.devel@gmail.com>
2022-11-28 15:30:39 +0200

    Update Clock.scala

--- clock/src/main/scala/Clock.scala
+++ clock/src/main/scala/Clock.scala
@@ -2,21 +2,22 @@ object Clock {
 
     case class Clock( h:Int, m:Int ) {
 
-        private def mod(x:Double, d:Double) = (((x % d) + d) % d).toInt 
+        private def mod(x:Double, d:Double) = 
+            (((x % d) + d) % d).toInt 
 
         private lazy val hours = mod((h * 60 + m) / 60.0 , 24)
         private lazy val minutes = mod(m, 60)
 
-        def + (other:Clock):Clock = new Clock(hours + other.hours, 
-                                              minutes + other.minutes)
-        def - (other:Clock):Clock = new Clock(hours - other.hours, 
-                                              minutes - other.minutes)  
-        override def equals(other:Any):Boolean = {
-            other match {
-                case other:Clock => 
-                    other.isInstanceOf[Clock] && 
-                    other.hours == this.hours &&
-                    other.minutes == this.minutes
+        def + (other:Clock):Clock = 
+            new Clock(hours + other.hours, minutes + other.minutes)
+        def - (other:Clock):Clock = 
+            new Clock(hours - other.hours, minutes - other.minutes)
+  
+        override def equals(that:Any):Boolean = {
+            that match {
+                case that:Clock => 
+                    that.isInstanceOf[Clock] && 
+                    that.hours == this.hours && that.minutes == this.minutes
                 case _ => 
                     false
             }
@@ -24,6 +25,5 @@ object Clock {
     }
 
     def apply(hours: Int, minutes: Int) = new Clock(hours, minutes)
-    
     def apply(minutes: Int) = new Clock(0, minutes)
 }
