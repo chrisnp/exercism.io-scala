409b34846f2947eff25cfcbf22f86b14f5acd664
Invigo <invigo.devel@gmail.com>
2022-11-28 15:49:58 +0200

    Update Series.scala

--- series/src/main/scala/Series.scala
+++ series/src/main/scala/Series.scala
@@ -1,13 +1,17 @@
 object Series {
     import scala.language.postfixOps
+    
     def slices(size: Int, text: String): Seq[Seq[Int]] = {
-        if (text == "") return Seq()
+        if (text == "") 
+            return Seq()
         windows(text split("") map(_ toInt), size)
     }
 
     def windows[T](list: Seq[T], size: Int): Seq[Seq[T]] = {
         val window = list take(size)
-        if ((window length) < size) Seq()
-        else window +: windows(list tail, size)
+        if ((window length) < size) 
+            Seq()
+        else 
+            window +: windows(list tail, size)
     }
 }
