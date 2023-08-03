5a400c1562938a97fd2abd8c3b9f548aad8dbad3
Invigo <invigo.devel@gmail.com>
2023-08-03 19:56:56 +0300

    Refactor CollatzConjecture

--- collatz-conjecture/src/main/scala/CollatzConjecture.scala
+++ collatz-conjecture/src/main/scala/CollatzConjecture.scala
@@ -1,8 +1,10 @@
+import scala.language.postfixOps
+
 object CollatzConjecture {
-    
+
     def steps(number: Int): Option[Int] = 
-        Option( number ) filter( _ > 0 ) map( iter(_) takeWhile(_ != 1) length )
+        Option(number) filter(_ > 0) map(iter(_) takeWhile(_ != 1) length)
 
     private def iter(num: Int): Stream[Int] =
-        Stream.iterate(num) ( n => if (n % 2 == 0) n / 2 else (n * 3) + 1 )
+        Stream.iterate(num) (n => if (n % 2 == 0) n >>> 1 else n * 3 + 1)
 }
