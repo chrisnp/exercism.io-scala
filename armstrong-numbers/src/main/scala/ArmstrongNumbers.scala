3dd543cb36c14e8726a21ed810f0e9e7d45cb17b
Invigo <invigo.devel@gmail.com>
2023-08-04 19:11:27 +0300

    Refactor ArmstrongNumbers

--- armstrong-numbers/src/main/scala/ArmstrongNumbers.scala
+++ armstrong-numbers/src/main/scala/ArmstrongNumbers.scala
@@ -1,6 +1,8 @@
 object ArmstrongNumbers {
     def isArmstrongNumber(num: Int): Boolean = {
         num == 
-        num.toString().map(_ asDigit).map(Math.pow(_, num.toString.length) toInt).sum
+        num.toString().map {_ asDigit}
+           .map {Math.pow(_, num.toString.length) toInt}
+           .foldLeft(0) {_+_}
     }
 }
