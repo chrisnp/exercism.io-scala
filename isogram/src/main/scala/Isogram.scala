76be8f42f60e18eb84979b88bd1269b10f0b3c45
Invigo <invigo.devel@gmail.com>
2023-10-20 01:36:21 +0300

    Reformat Isogram

--- isogram/src/main/scala/Isogram.scala
+++ isogram/src/main/scala/Isogram.scala
@@ -1,6 +1,6 @@
 object Isogram {
     def isIsogram(str: String): Boolean = {
         val sanitized = str filter(_ isLetter) map(_ toLower)
-        sanitized.toSet.size == sanitized.size
+        (sanitized.toSet size) == (sanitized size)
     }
 }
