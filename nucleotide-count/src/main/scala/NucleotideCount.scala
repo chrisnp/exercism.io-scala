f0c7a5abf8f2e39e8ca992cd9f152a3306ddc198
Invigo <invigo.devel@gmail.com>
2023-10-24 14:34:40 +0300

    Reformat NucleotideCount

--- nucleotide-count/src/main/scala/NucleotideCount.scala
+++ nucleotide-count/src/main/scala/NucleotideCount.scala
@@ -1,12 +1,10 @@
 case class DNA(sequence : String) {
-  
-    val nucleotideCounts : Either[String, Map[Char, Int]] = 
-    {   
+    val nucleotideCounts : Either[String, Map[Char, Int]] = {   
         if (sequence forall("ACGT" contains(_))) 
-            Right( Map('A' -> 0,'C' -> 0,'G' -> 0,'T' -> 0) 
+            Right (Map('A' -> 0,'C' -> 0,'G' -> 0,'T' -> 0) 
                    ++ 
-                   sequence.groupBy(identity).mapValues(_ size) )
+                   sequence.groupBy(identity).mapValues(_ size))
         else
-            Left( "Invalid nucleotide(s) in sequence" )
+            Left ("Invalid nucleotide(s) in sequence")
     }
 }
