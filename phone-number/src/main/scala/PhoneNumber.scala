cd14f8525b6c4b46dee550b1ab4c60bcccf252ca
Invigo <invigo.devel@gmail.com>
2024-02-27 18:11:52 +0200

    Update PhoneNumber.scala

--- phone-number/src/main/scala/PhoneNumber.scala
+++ phone-number/src/main/scala/PhoneNumber.scala
@@ -1,14 +1,15 @@
 object PhoneNumber {
     def clean(number: String): Option[String] = {
         val digits = number.filter(_.isDigit).toList
-        val areaCodeOk = digits(0) > 1
-        val exchCodeOk = digits(3) > 1
+        val areaCodeOk = digits.head.asDigit > 1
+        val exchCodeOk = digits(3).asDigit > 1
         digits match {
-            case '1' :: rest if digits.size == 11 => clean(rest.mkString)
-            case listDigits if listDigits.size == 10 && 
-                 listDigits.head.asDigit > 1 && 
-                 listDigits(3).asDigit > 1 => Some(listDigits.mkString)
-            case _ => None
+            case '1' :: rest if digits.size == 11 => 
+                clean(rest.mkString)
+            case digs if digs.size == 10 && areaCodeOk && exchCodeOk => 
+                Some(digs.mkString)
+            case _ => 
+                None
         }
     }
 }
