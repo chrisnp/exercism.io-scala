2685de22f748ff9ffc90e8400e74a383d60bf110
Invigo <invigo.devel@gmail.com>
2024-04-26 18:18:38 +0300

    Create PascalsTriangle.scala

---
+++ pascals-triangle/src/main/scala/PascalsTriangle.scala
@@ -0,0 +1,10 @@
+object PascalsTriangle {
+    def rows(count: Int): Seq[Seq[Int]] = {
+        (0 until count).foldRight(Seq[Seq[Int]]()) { 
+            (x, acc) => (0 until x).foldLeft(Seq(1)) { 
+                (r, y) => r.appended(r(y) * (x - y) / (y + 1))
+            }
+            .toSeq +: acc
+        }
+    }
+}
