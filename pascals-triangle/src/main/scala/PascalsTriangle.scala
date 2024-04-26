b8b8de1c742fb3dea380e6f5618d3ba3f141be3f
Invigo <invigo.devel@gmail.com>
2024-04-26 18:21:35 +0300

    Update PascalsTriangle.scala

--- pascals-triangle/src/main/scala/PascalsTriangle.scala
+++ pascals-triangle/src/main/scala/PascalsTriangle.scala
@@ -3,8 +3,7 @@ object PascalsTriangle {
         (0 until count).foldRight(Seq[Seq[Int]]()) { 
             (x, acc) => (0 until x).foldLeft(Seq(1)) { 
                 (r, y) => r.appended(r(y) * (x - y) / (y + 1))
-            }
-            .toSeq +: acc
+            }.toSeq +: acc
         }
     }
 }
