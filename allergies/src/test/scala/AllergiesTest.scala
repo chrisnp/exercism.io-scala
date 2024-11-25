ca4073c047ea95b7858666316709792d04105ad5
Invigo <invigo.devel@gmail.com>
2024-11-25 21:48:02 +0200

    Update AllergiesTest.scala

--- allergies/src/test/scala/AllergiesTest.scala
+++ allergies/src/test/scala/AllergiesTest.scala
@@ -10,87 +10,87 @@ class AllergiesTest extends AnyFunSuite with Matchers {
   }
 
   test("Allergen.Cats - no allergies means not allergic") {
-    pending
+    // pending
     Allergies.allergicTo(Allergen.Cats, 0) should be(false)
   }
 
   test("Allergen.Strawberries - no allergies means not allergic") {
-    pending
+    // pending
     Allergies.allergicTo(Allergen.Strawberries, 0) should be(false)
   }
 
   test("Allergen.Eggs - is allergic to eggs") {
-    pending
+    // pending
     Allergies.allergicTo(Allergen.Eggs, 1) should be(true)
   }
 
   test("Allergen.Eggs - allergic to eggs in addition to other stuff") {
-    pending
+    // pending
     Allergies.allergicTo(Allergen.Eggs, 5) should be(true)
   }
 
   test("Allergen.Shellfish - allergic to eggs in addition to other stuff") {
-    pending
+    // pending
     Allergies.allergicTo(Allergen.Shellfish, 5) should be(true)
   }
 
   test("Allergen.Strawberries - allergic to eggs in addition to other stuff") {
-    pending
+    // pending
     Allergies.allergicTo(Allergen.Strawberries, 5) should be(false)
   }
 
   test("Allergen.Eggs - allergic to strawberries but not peanuts") {
-    pending
+    // pending
     Allergies.allergicTo(Allergen.Eggs, 9) should be(true)
   }
 
   test("Allergen.Peanuts - allergic to strawberries but not peanuts") {
-    pending
+    // pending
     Allergies.allergicTo(Allergen.Peanuts, 9) should be(false)
   }
 
   test("Allergen.Shellfish - allergic to strawberries but not peanuts") {
-    pending
+    // pending
     Allergies.allergicTo(Allergen.Shellfish, 9) should be(false)
   }
 
   test("Allergen.Strawberries - allergic to strawberries but not peanuts") {
-    pending
+    // pending
     Allergies.allergicTo(Allergen.Strawberries, 9) should be(true)
   }
 
   test("no allergies at all") {
-    pending
+    // pending
     Allergies.list(0) should be(List())
   }
 
   test("allergic to just eggs") {
-    pending
+    // pending
     Allergies.list(1) should be(List(Allergen.Eggs))
   }
 
   test("allergic to just peanuts") {
-    pending
+    // pending
     Allergies.list(2) should be(List(Allergen.Peanuts))
   }
 
   test("allergic to just strawberries") {
-    pending
+    // pending
     Allergies.list(8) should be(List(Allergen.Strawberries))
   }
 
   test("allergic to eggs and peanuts") {
-    pending
+    // pending
     Allergies.list(3) should be(List(Allergen.Eggs, Allergen.Peanuts))
   }
 
   test("allergic to more than eggs but not peanuts") {
-    pending
+    // pending
     Allergies.list(5) should be(List(Allergen.Eggs, Allergen.Shellfish))
   }
 
   test("allergic to lots of stuff") {
-    pending
+    // pending
     Allergies.list(248) should be(
       List(Allergen.Strawberries,
            Allergen.Tomatoes,
@@ -100,7 +100,7 @@ class AllergiesTest extends AnyFunSuite with Matchers {
   }
 
   test("allergic to everything") {
-    pending
+    // pending
     Allergies.list(255) should be(
       List(Allergen.Eggs,
            Allergen.Peanuts,
@@ -113,7 +113,7 @@ class AllergiesTest extends AnyFunSuite with Matchers {
   }
 
   test("ignore non allergen score parts") {
-    pending
+    // pending
     Allergies.list(509) should be(
       List(Allergen.Eggs,
            Allergen.Shellfish,
